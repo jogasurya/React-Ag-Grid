a05701a8003f6c1703a4917672267e7c83e89e05
jogasurya <107780867+jogasurya@users.noreply.github.com>
2023-04-06 17:25:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+const columns = [
+  { field: "id", headerName: "ID" },
+  { field: "title", headerName: "Title" },
+  { field: "price", headerName: "Price" },
+  { field: "brand", headerName: "Brand" },
+  { field: "stock", headerName: "Stock" }
+];
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((json) => setData(json.products));
+  }, []);
+  console.log("data", data);
+  return (
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={data} columnDefs={columns}></AgGridReact>
+    </div>
+  );
+}
